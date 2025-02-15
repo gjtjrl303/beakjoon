ad22a63d72cf5b45f75f76cc46373ec2747cde1d
gjtjrl303 <154502567+gjtjrl303@users.noreply.github.com>
2025-02-15 21:33:15 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include<stdlib.h>
+#pragma warning(disable : 4996)
+
+int main() {
+
+	int A, B;
+	scanf("%d %d", &A, &B);
+	printf("%d", A * B);
+	return 0;
+}

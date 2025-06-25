ff130064a1f02b4833aa054e5ccd5475cf701eab
gjtjrl303 <154502567+gjtjrl303@users.noreply.github.com>
2025-06-25 21:20:44 +0900

    [Silver III] Title: N과 M (4), Time: 228 ms, Memory: 20760 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    static int N, M;
+    static Scanner scanner = new Scanner(System.in);
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            int count = 0;
+            StringBuilder sb = new StringBuilder().append(i + " ");
+            recursion(i, sb, count + 1);
+        }
+        bw.flush();
+    }
+
+    private static void recursion(int i,StringBuilder sb, int count) throws IOException {
+
+        if (count == M) {
+            bw.write(sb + "\n");
+            return;
+        }
+        for (int j = i; j <= N; j++) {
+            StringBuilder newSb = new StringBuilder(sb);
+            newSb.append(j + " ");
+
+            recursion(j, newSb, count + 1);
+        }
+    }
+}

a27c013044a80bfe09607146caf345abbb3f57b2
gjtjrl303 <154502567+gjtjrl303@users.noreply.github.com>
2025-06-26 16:12:24 +0900

    [Silver III] Title: 다음 순열, Time: 312 ms, Memory: 20200 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.java
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -1,87 +1,53 @@
+
 import java.io.*;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static boolean[] used;
-    static int[] targetArray;
-    static int[] array;
-    static int N;
-    static boolean search = false;
-    static boolean flag = false;
-
     public static void main(String[] args) throws IOException {
 
+        int N;
+        int[] array;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
         array = new int[N];
-        targetArray = new int[N];
-        used = new boolean[N];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            targetArray[i] = Integer.parseInt(st.nextToken());
+            array[i] = Integer.parseInt(st.nextToken());
         }
-        array = Arrays.copyOf(targetArray, N);
-        Arrays.sort(array);
 
+        int i = N - 1;
+        boolean flag = false;
 
-        for (int i = 0; i < N; i++) {
-            int index = 0;
-            if (array[i] >= targetArray[index]) {
-                used[i] = true;
-                StringBuilder sb = new StringBuilder();
-                sb.append(array[i] + " ");
-                recursion(index + 1, sb);
-                used[i] = false;
+        for (; i > 0; i--) {
+            if (array[i - 1] < array[i]) {
+                int min = 100000;
+                int minIndex = 0;
+                for (int j = N - 1; j >= i; j--) {
+                    if (array[j] < min && array[j] > array[i - 1]) {
+                        min = array[j];
+                        minIndex = j;
+                    }
+                }
+                int temp = array[i - 1];
+                array[i - 1] = min;
+                array[minIndex] = temp;
+                flag = true;
+                break;
             }
         }
+        Arrays.sort(array, i, N);
 
         if (!flag) {
             System.out.println(-1);
         } else {
-            bw.flush();
-        }
-    }
-
-    private static void recursion(int index, StringBuilder sb) throws IOException {
-
-        if (flag) {
-            return;
-        }
-
-        if (index == N && search) {
-            bw.write(String.valueOf(sb));
-            flag = true;
-        }
-        if (index == N) {
-            search = true;
-            return;
-        }
-
-        for (int i = 0; i < N; i++) {
-            if (!search) {
-                if (array[i] == targetArray[index]) {
-                    used[i] = true;
-                    StringBuilder newSt = new StringBuilder(sb);
-                    newSt.append(array[i] + " ");
-                    recursion(index + 1, newSt);
-                    used[i] = false;
-                }
-            } else {
-                if (!used[i]) {
-                    used[i] = true;
-                    StringBuilder newSt = new StringBuilder(sb);
-                    newSt.append(array[i] + " ");
-                    recursion(index + 1, newSt);
-                    used[i] = false;
-                }
-            }
-            if (flag) {
-                return;
+            for (int j = 0; j < N; j++) {
+                System.out.print(array[j] + " ");
             }
         }
     }

9458d8cfa9daf4ee125f6d4e0e3f8a015d29ddc4
gjtjrl303 <154502567+gjtjrl303@users.noreply.github.com>
2025-06-26 20:40:36 +0900

    [Silver III] Title: N과 M (7), Time: 792 ms, Memory: 228904 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,50 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[] array;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        array = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(array);
+
+        for (int i = 0; i < N; i++) {
+            int count = 0;
+            StringBuilder sb = new StringBuilder().append(array[i] + " ");
+            recursion(sb, count + 1);
+            bw.flush();
+        }
+
+        bw.flush();
+    }
+
+    private static void recursion(StringBuilder sb, int count) throws IOException {
+
+        if (count == M) {
+            bw.write(sb + "\n");
+            return;
+        }
+        for (int j = 0; j < N; j++) {
+                StringBuilder newSb = new StringBuilder(sb);
+                newSb.append(array[j] + " ");
+                recursion(newSb, count + 1);
+        }
+    }
+}

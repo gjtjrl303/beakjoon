f0b6dd3612f6926fe3278bb38f8c1422042e9598
gjtjrl303 <154502567+gjtjrl303@users.noreply.github.com>
2025-07-08 11:54:09 +0900

    [Gold V] Title: 링크와 스타트, Time: 1304 ms, Memory: 18984 KB -BaekjoonHub

---
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -0,0 +1,72 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+    static int[][] num;
+    static int[] team;
+    static int N;
+    static int sum;
+    static int count = 0;
+    static int min = 1000000;
+    static int teamCount;
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        num = new int[N][N];
+        team = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                num[i][j] = scanner.nextInt();
+                sum += num[i][j];
+            }
+        }
+
+        for (teamCount = 1; teamCount <= N / 2; teamCount++) {
+            for (int i = 0; i < N; i++) {
+                team[i] = 1;
+                count++;
+                dfs(i);
+                team[i] = 0;
+                count--;
+            }
+        }
+        System.out.println(min);
+    }
+
+    static void dfs(int start) {
+        if (count == teamCount) {
+            calculateSum();
+        }
+
+        for (int i = start + 1; i < N; i++) {
+            team[i] = 1;
+            count++;
+            dfs(i);
+            team[i] = 0;
+            count--;
+        }
+    }
+
+    static void calculateSum() {
+        int teamSum = 0;
+        int otherTeam = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    continue;
+                }
+                if (team[i] == team[j] && team[i] == 1) {
+                    teamSum += num[i][j];
+                }
+                if (team[i] == team[j] && team[i] == 0) {
+                    otherTeam += num[i][j];
+                }
+            }
+        }
+        min = Math.min(min, Math.abs(otherTeam - teamSum));
+    }
+}
